f1cfb61df9789f772e2b4fc7d1755bedceb95ff7
Michael Schiumo <mschiumo18@gmail.com>
2019-03-15 20:02:45 -0400

    setting up methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,31 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
   end
 
   get '/' do
     "Welcome to Fwitter!"
-    erb :index
   end
 
+  get '/signup' do
+    erb :'signup'
+  end
 
+  post '/signup' do
+    @user = User.create(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:user_id] = @user.id
+    puts params
 
+    redirect to '/twitter_index'
+  end
+
+  get '/login' do
+
+  end
+
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end  
 end
