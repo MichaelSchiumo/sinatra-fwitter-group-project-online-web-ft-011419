ed0b7af8d39a4e791304e6e7f6ae008b22c964b2
Michael Schiumo <mschiumo18@gmail.com>
2019-04-02 16:23:06 -0400

    controller work

--- app/controllers/models/user.rb
+++ app/controllers/models/user.rb
@@ -3,5 +3,15 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, :username, :email, presence: true
 
+  def slug
+    slug = self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
 
+  def self.find_by_slug(slug)
+    User.all.find do |user|
+      if user.slug == slug
+        user
+      end
+    end
+  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,4 +14,28 @@ class TweetsController < ApplicationController
       redirect to '/login'
     end
   end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.new(params)
+
+    if @tweet.content = ""
+      redirect to '/tweets/new'
+    elsif @tweet.save
+      @user = current_user(session)
+      @user.tweets << @tweet
+      redirect to '/tweets'
+    else
+      redirect to '/signup'  
+    end
+  end
+
+
 end
